8f65802ae5edf3f9d47a6145ec66a0f504a5c974
ronnakorn <ronnda@kku.ac.th>
2024-06-03 15:39:37 +0700

    pastel

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { db } from './firebaseConfig';
 import { collection, addDoc, getDoc, query, where, getDocs, updateDoc, doc } from 'firebase/firestore';
+import Select from 'react-select';
 
 function App() {
   let now = new Date();
@@ -15,6 +16,7 @@ function App() {
   const [endDate, setEndDate] = useState(nowtext);
   const [summaryResult, setSummaryResult] = useState();
   const [summaryMembers, setSummaryMembers] = useState([]);
+  const [summaryValue, setSummaryValue] = useState(0);
   const defaultMember = ([
     {
       name: "พรรณทิพา",
@@ -105,10 +107,16 @@ function App() {
       name: "อารียา",
       value: 0,
       paid: false
+    },
+    {
+      name: "ชยวรรธย์",
+      value: 0,
+      paid: false
     }
   ]);
   const [currentMember, setCurrentMember] = useState(defaultMember);
   const [currentPage, setCurrentPage] = useState("entry");
+  const options = currentMember.map(member => ({ value: member.name, label: member.name }));
 
   useEffect(() => {
     fetchData();
@@ -152,6 +160,17 @@ function App() {
     setCurrentDate(date.toISOString().split('T')[0]);
   }
 
+  const [selectedMember, setSelectedMember] = useState(null);
+
+  const memberOptions = currentMember.map(member => ({
+    value: member.name,
+    label: member.name
+  }));
+
+  const handleSelect = (selectedOption) => {
+    setSelectedMember(selectedOption);
+    console.log("Selected Member:", selectedOption);
+  };
 
   const handleChange = (name, value) => {
     let member = currentMember;
@@ -213,8 +232,8 @@ function App() {
               twohour: member.value == 200 ? 1 : 0,
               absent: member.value == 300 ? 1 : 0,
               forgot: member.value == 99 ? 1 : 0,
-              value: (member.value != 99 && member.value != 101) ? member.value:0,
-              paid:  member.paid
+              value: (member.value != 99 && member.value != 101) ? member.value : 0,
+              paid: member.paid
             });
           } else {
             let index = members.findIndex(item => item.name == member.name);
@@ -235,23 +254,28 @@ function App() {
             if (member.value != 99 && member.value != 101) {
               members[index].value += member.value;
             }
-            if(member.paid == false){
-               members[index].paid = false;
+            if (member.paid == false) {
+              members[index].paid = false;
             }
           }
         }
         );
       });
       setSummaryMembers([...members]);
+      let sumvalue = 0;
+      members.forEach(member => {
+        sumvalue += member.value;
+      });
+      setSummaryValue(sumvalue);
       setSummaryResult([...docs]);
     } catch (e) {
       console.log("error summarize", e);
     }
   }
 
-  const togglePaid = async(name,paid) =>{
+  const togglePaid = async (name, paid) => {
     let allDocs = summaryResult;
-    await allDocs.forEach(async(item) =>{
+    await allDocs.forEach(async (item) => {
       let members = item.data.members;
       let index = members.findIndex(member => member.name == name);
       members[index].paid = !paid;
@@ -264,7 +288,7 @@ function App() {
       } catch (e) {
         console.error("Error adding document: ", e);
       }
-       }
+    }
     )
     summarize();
   }
@@ -284,8 +308,18 @@ function App() {
             <input className='input  p-3 border-2 input-bordered' type="date" value={currentDate} onChange={(e) => setCurrentDate(e.target.value)} />
             <button className='btn btn-xs btn-primary' onClick={nextDate}>Next</button>
           </div>
+          <div className='mt-5 w-3/5 flex justify-center items-center gap-2'>
+            <Select
+              className='w-full'
+              value={selectedMember}
+              onChange={handleSelect}
+              options={memberOptions}
+              placeholder="Select a member"
+            />
+            <button className='btn btn-square' onClick={() => setSelectedMember(null)}>Clear</button>
+          </div>
           <div className=' overflow-x-auto md:w-full'>
-            <table className='table-xs table-pin-cols  md:table-md table'>
+            <table className='table-xs md:table-md table'>
               <tr>
                 <th></th>
                 <th className='text-xs text-center border-l-2 border-b-2'>9.00-9.15<br></br>13.00-13.15</th>
@@ -294,29 +328,28 @@ function App() {
                 <th className='text-xs text-center border-b-2'>10.01-12.00<br></br>14.01-16.00</th>
                 <th className='text-xs text-center border-b-2'>ไม่มา</th>
                 <th className='text-xs text-center border-b-2'>ลืมลงชื่อ</th>
-                <th className='text-xs text-center border-b-2'>เวรอื่น</th>
                 <th className='text-xs text-center border-b-2'>สรุป</th>
               </tr>
               {currentMember.map(member => {
-                return (
-                  <tr>
-                    <td className='text-xs'>
-                      {member.name}
-                    </td>
-                    <td className='text-center  border-b-2 border-l-2'><input type="radio" className='radio radio-xs' onChange={() => handleChange(member.name, 0)} name={member.name} id="" checked={member.value == 0} /></td>
-                    <td className='text-center  border-b-2'><input type="radio" className=' radio radio-xs'  onChange={() => handleChange(member.name, 50)} name={member.name} id="" checked={member.value == 50} /></td>
-                    <td className='text-center  border-b-2'><input type="radio" className=' radio radio-xs'  onChange={() => handleChange(member.name, 100)} name={member.name} id="" checked={member.value == 100} /></td>
-                    <td className='text-center  border-b-2'><input type="radio" className=' radio radio-xs'  onChange={() => handleChange(member.name, 200)} name={member.name} id="" checked={member.value == 200} /></td>
-                    <td className='text-center  border-b-2'><input type="radio" className=' radio radio-xs' onChange={() => handleChange(member.name, 300)} name={member.name} id="" checked={member.value == 300} /></td>
-                    <td className='text-center  border-b-2'><input type="radio" className=' radio radio-xs' onChange={() => handleChange(member.name, 99)} name={member.name} id="" checked={member.value == 99} /></td>
-                    <td className='text-center  border-b-2'><input type="radio" className=' radio radio-xs' onChange={() => handleChange(member.name, 101)} name={member.name} id="" checked={member.value == 101} /></td>
-                    <td className='text-center  border-b-2 bg-slate-300 text-black'>{member.value == 99 ||  member.value == 101 ? 0 : member.value}</td>
-                  </tr>
-                )
+                if (member.name.includes(selectedMember ? selectedMember.label : ""))
+                  return (
+                    <tr>
+                      <td className='text-xs text-center'>
+                        {member.name}
+                      </td>
+                      <td className='text-center text-xs  border-b-2 border-l-2'><input type="radio" className='radio radio-xs' onChange={() => handleChange(member.name, 0)} name={member.name} id="" checked={member.value == 0} /></td>
+                      <td className='text-center text-xs  border-b-2'><input type="radio" className=' radio radio-xs' onChange={() => handleChange(member.name, 50)} name={member.name} id="" checked={member.value == 50} /></td>
+                      <td className='text-center text-xs  border-b-2'><input type="radio" className=' radio radio-xs' onChange={() => handleChange(member.name, 100)} name={member.name} id="" checked={member.value == 100} /></td>
+                      <td className='text-center text-xs  border-b-2'><input type="radio" className=' radio radio-xs' onChange={() => handleChange(member.name, 200)} name={member.name} id="" checked={member.value == 200} /></td>
+                      <td className='text-center text-xs  border-b-2'><input type="radio" className=' radio radio-xs' onChange={() => handleChange(member.name, 300)} name={member.name} id="" checked={member.value == 300} /></td>
+                      <td className='text-center text-xs  border-b-2'><input type="radio" className=' radio radio-xs' onChange={() => handleChange(member.name, 99)} name={member.name} id="" checked={member.value == 99} /></td>
+                      <td className='text-center text-xs  border-b-2 bg-slate-300 text-black'>{member.value == 99 || member.value == 101 ? 0 : member.value}</td>
+                    </tr>
+                  )
               })
               }
               <tr>
-                <td colSpan={8} ></td>
+                <td colSpan={7} ></td>
                 <td className='text-center  text-black bg-slate-400'>{summary}</td>
               </tr>
             </table>
@@ -340,7 +373,7 @@ function App() {
             <button className='btn btn-primary capitalize btn-sm' onClick={summarize}>summary</button>
           </div>
           <div className='w-full'>
-            <table className='table table-xs'>
+            <table className='table table-xs md:table-md'>
               <tr>
                 <th></th>
                 <th className='text-xs text-center  border-b-2'>มาสาย</th>
@@ -349,17 +382,25 @@ function App() {
                 <th className='text-xs text-center border-b-2'>สรุป</th>
                 <th className='text-xs text-center border-b-2'>สถานะ</th>
               </tr>
-              {summaryMembers.map(member =>
+              {summaryMembers.sort((a, b) => {
+                const sumA = a.thirty + a.onehour + a.twohour + a.absent;
+                const sumB = b.thirty + b.onehour + b.twohour + b.absent;
+                return sumB - sumA; // Sort in descending order
+              }).map(member =>
                 <tr>
-                <td className='text-xs text-center border-b-2'>{member.name}</td>
-                <td className='text-xs text-center border-b-2'>{member.thirty+member.onehour+member.twohour}</td>
-                <td className='text-xs text-center border-b-2'>{member.absent}</td>
-                <td className='text-xs text-center border-b-2'>{member.forgot}</td>
-                <td className='text-xs text-center border-b-2 bg-slate-300 text-black'>{member.value}</td>
-                <td className='text-xs text-center border-b-2'><button onClick={()=>togglePaid(member.name,member.paid)} className={`btn btn-xs text-black ${member.paid? "bg-green-500":"bg-red-500"}`}>{member.paid? "จ่ายแล้ว":"ยังไม่จ่าย"}</button></td>
+                  <td className='text-xs text-center border-b-2'>{member.name}</td>
+                  <td className='text-xs text-center border-b-2'>{member.thirty + member.onehour + member.twohour}</td>
+                  <td className='text-xs text-center border-b-2'>{member.absent}</td>
+                  <td className='text-xs text-center border-b-2'>{member.forgot}</td>
+                  <td className='text-xs text-center border-b-2 bg-slate-300 text-black'>{member.value}</td>
+                  <td className='text-xs text-center border-b-2'><button onClick={() => togglePaid(member.name, member.paid)} className={`btn btn-xs text-black ${member.paid ? "bg-green-500" : "bg-red-500"}`}>{member.paid ? "จ่ายแล้ว" : "ยังไม่จ่าย"}</button></td>
                 </tr>
               )
               }
+              <tr>
+                <td colSpan={4} ></td>
+                <td className='text-center text-xs bg-slate-500'>{summaryValue}</td>
+              </tr>
             </table>
           </div>
         </>
